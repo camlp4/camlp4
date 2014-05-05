bdac4220af72b7353034e1bc43a1c3b7bf30381f
Anil Madhavapeddy <anil@recoil.org>
2014-05-05 14:04:42 +0100

    Add Travis files, based on the OCaml ones

---
+++ .travis-ci.sh
@@ -0,0 +1,16 @@
+case $XARCH in
+i386)
+  uname -a
+  git clone git://github.com/ocaml/ocaml
+  cd ocaml
+  ./configure
+  make world.opt
+  sudo make install
+  cd ..
+  ./configure && make && sudo make install
+  ;;
+*)
+  echo unknown arch
+  exit 1
+  ;;
+esac
