c7d27100a1bd70cf7729646dc5de5d08d22b610d
Jacques Garrigue <garrigue@math.nagoya-u.ac.jp>
2014-11-12 20:55:58 +0900

    add Osig_ellipsis for trunk

--- camlp4/Camlp4Top/Rprint.ml
+++ camlp4/Camlp4Top/Rprint.ml
@@ -368,7 +368,8 @@ and needs_semi =
   | Osig_type _ rs -> rs <> Orec_next
   | Osig_typext _ _
   | Osig_modtype _ _
-  | Osig_value _ _ _ -> True ]
+  | Osig_value _ _ _ -> True
+  | Osig_ellipsis -> False ]
 and print_out_signature ppf =
   fun
   [ [] -> ()
@@ -425,7 +426,9 @@ and print_out_sig_item ppf =
             } ]
       in
       fprintf ppf "@[<2>%s %a :@ %a%a@]" kwd value_ident name
-        Toploop.print_out_type.val ty pr_prims prims ]
+        Toploop.print_out_type.val ty pr_prims prims
+  | Osig_ellipsis ->
+      fprintf ppf "..." ]
 
 and print_out_type_decl kwd ppf { otype_name    = name
                                 ; otype_params  = args
