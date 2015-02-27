1f68ac043937a9783714d7c8fd25bc1836c41aa9
Jeremie Dimino <jdimino@janestreet.com>
2015-02-27 17:01:26 +0000

    Fix headers
    s/top of the OCaml source tree/top of the Camlp4 source tree/

--- camlp4/Camlp4/Struct/Grammar/Print.ml
+++ camlp4/Camlp4/Struct/Grammar/Print.ml
@@ -7,7 +7,7 @@
 (*  Copyright  2006   Institut National de Recherche  en  Informatique et   *)
 (*  en Automatique.  All rights reserved.  This file is distributed under   *)
 (*  the terms of the GNU Library General Public License, with the special   *)
-(*  exception on linking described in LICENSE at the top of the OCaml       *)
+(*  exception on linking described in LICENSE at the top of the Camlp4      *)
 (*  source tree.                                                            *)
 (*                                                                          *)
 (****************************************************************************)
