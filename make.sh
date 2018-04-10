5fa7a517fb6d32b9a3e185d497d8def482cd92a1
Raja Boujbel <raja.boujbel@ocamlpro.com>
2018-04-10 18:20:14 +0200

    fix make.sh: exit 1 if campl4 fails

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-camlp4orf -v 2> /dev/null || (cat install && exit 1)
+camlp4orf -v 2> /dev/null || { cat install && exit 1; }
 
 version="`camlp4 -version`"
 sed -e "s/@@VERSION@@/$version/g" META.in > META
